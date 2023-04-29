b4ef3501f740717b030e6468a3ae0eec0eee4b72
vakulenkon <101878269+vakulenkon@users.noreply.github.com>
2023-04-29 17:56:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,16 @@ let score = 20;
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let highscore = 0;
 
+const btnCloseWindow = document.querySelector('.modal__close');
+const windowMsg = document.querySelector('.modal__msg');
+const hidden = document.querySelector('.hidden');
+const modal = document.querySelector('.modal');
+const background = document.querySelector('.background');
+
+// const open = () => {
+//   windowMsg.remove.classlist('hidden');
+// };
+
 //Helper Functions
 const message = msg => {
   document.querySelector('.message').textContent = msg;
@@ -11,6 +21,17 @@ const message = msg => {
 
 const scoreDisplay = curr =>
   (document.querySelector('.score').textContent = curr);
+
+const close = () => {
+  modal.classList.add('hidden');
+  background.classList.add('hidden');
+};
+
+const keydown = e => {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    close();
+  }
+};
 
 //1st See if Guess Exists. -> EVERY SCENARIO FOR GUESS
 document.querySelector('.check').addEventListener('click', function () {
@@ -25,6 +46,10 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = 'orangered';
     document.querySelector('.number').style.width = '30rem';
+    modal.classList.remove('hidden');
+    background.classList.remove('hidden');
+    document.addEventListener('keydown', keydown);
+
     //Update High Score . if curr>highScore then curr becomes highScore
     if (score > highscore) {
       highscore = score;
@@ -56,6 +81,9 @@ document.querySelector('.again').addEventListener('click', function () {
 
   message(' Start Guessing !');
   document.querySelector('.number').textContent = '?';
-
+  modal.classList.add('hidden');
+  background.classList.add('hidden');
   scoreDisplay(score);
 });
+
+btnCloseWindow.addEventListener('click', close);
